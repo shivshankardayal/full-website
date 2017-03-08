ba7f2e8b55d5a40f33813b1734883b6fc7682e19
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2017-03-08 20:25:32 +0530

    new design

--- dsa/source/conf.py
+++ dsa/source/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -91,8 +92,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
-#sys.path.append(os.path.abspath('_themes'))
-#html_theme_path = ['_themes']
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
 #html_theme = 'bootstrap'
 #html_theme = 'bootstrap'
 #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
